68ab0cff7fe5b46e0b6c9396a43e1a99e52780c7
alireza1420 <alirezafaghihi1383@gmail.com>
2025-05-24 09:44:12 +0200

    new train and playbook

--- 0_ci_cd/development_server/train.py
+++ 0_ci_cd/development_server/train.py
@@ -365,8 +365,8 @@ def train_and_evaluate_models(df, save_path="best_model_cv.pkl"):
     print(results_df.to_string())
 
     if best_model:
+        save_path = "/output/best_model.pkl"
         joblib.dump(best_model, save_path)
-        #print(f"\n Best model {best_model: <30} ({best_score: <30})")
         print(f"\n Best model saved to: {os.path.abspath(save_path)}")
     
     return results_df
