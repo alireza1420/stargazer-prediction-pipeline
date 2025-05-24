bc95ea0571416048a308312df01f347c7eb5fc1e
alireza1420 <alirezafaghihi1383@gmail.com>
2025-05-24 11:32:45 +0200

    changes in files

--- 0_ci_cd/development_server/train.py
+++ 0_ci_cd/development_server/train.py
@@ -365,8 +365,9 @@ def train_and_evaluate_models(df, save_path="best_model_cv.pkl"):
     print(results_df.to_string())
 
     if best_model:
-        os.makedirs("output", exist_ok=True)
-        save_path = "/output/best_model.pkl"
+        os.makedirs("/app/output", exist_ok=True)
+        save_path = "/app/output/best_model.pkl"
+        joblib.dump(best_model, save_path)
         joblib.dump(best_model, save_path)
         print(f"\n Best model saved to: {os.path.abspath(save_path)}")
     
